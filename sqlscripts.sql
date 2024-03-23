3449410ccaafe746bf87a802a0589c7150db62d0
Mirza Muhammad Aqeel <mmaqeel@pk.ibm.com>
2024-03-23 22:11:09 +0500

    database scripts - user table only

---
+++ sqlscripts.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `e_portal`.`users` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(250) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `pwd` VARCHAR(45) NOT NULL,
+  `phone` VARCHAR(45) NULL,
+  `roleId` VARCHAR(45) NOT NULL,
+  `status` TINYINT NOT NULL DEFAULT 0,
+  `created_at` DATETIME NULL,
+  `updated_at` DATETIME NULL,
+  PRIMARY KEY (`id`));
+  
+  
+ 
